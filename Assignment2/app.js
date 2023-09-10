aec4322907695e5ef0436cf2868b7e19546fbefc
Irshad-Mehsud <144600021+Irshad-Mehsud@users.noreply.github.com>
2023-09-10 18:56:51 +0500

    Add files via upload

---
+++ Assignment2/app.js
@@ -0,0 +1,49 @@
+//1.Declare a variable called username//===========================================
+var username;
+//=================================================================================
+//2.Declare a variable called myName & assign to it a string 
+//that represents your Full Name
+var myName="Irshad Ahmad"
+//=================================================================================
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+var message="Hello World";
+alert(message)
+//=================================================================================
+// 4.Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+var Name="Jhon Doe";
+var age="15 years old";
+var skil="Certified Mobile Application Development";
+alert(Name)
+alert(age)
+alert(skil)
+//=================================================================================
+//5.Write a script to display the following alert using one JS 
+//variable:
+var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza)
+//=================================================================================
+// 6.Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+var email="irshadahmad19059@gmail.com";
+alert("My email Address is"+" "+email)
+//==================================================================================
+//7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book="A smarter way to learn JavaScript";
+alert(book)
+//==================================================================================
+// 8.Write a script to display this in browser through Js
+document.write("Yah1 I can write HTML content through JavaScript\n")
+//==================================================================================
+//Q.9)
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design)
+//==================================THE END=========================================
