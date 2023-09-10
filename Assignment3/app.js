582b538a079256132135db37a4d85670675dbda5
Irshad-Mehsud <144600021+Irshad-Mehsud@users.noreply.github.com>
2023-09-10 18:57:45 +0500

    Add files via upload

---
+++ Assignment3/app.js
@@ -0,0 +1,32 @@
+//======================================Q1======================================================
+// 1.Declare a variable called age & assign to it your age. Show
+//your age in an alert box.
+var age=21;
+alert("I am "+age+" years old.")
+//=======================================Q2=======================================================
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var vissit=10;
+alert("You have visited this site "+vissit+" times")
+//=========================================Q3=====================================================
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+var birthYear=2003;
+document.write("My birth year is "+birthYear+"<br/>Datatype of my declared variable is "+typeof(birthYear));
+//=========================================Q4======================================================
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var Name="Irshad Ahmad";
+var Product="T-shirt";
+var Quantity=15;
+document.write( "<br/>"+ Name+" ordered "+Quantity+" " +Product+" on Bonanza Clothing Store.")
+//=========================================THE END===============================================
